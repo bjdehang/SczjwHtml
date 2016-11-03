3d72a1aa060d4f1fbda64711a0ece1d8b02ed1b4
bjdehang <bjdehang@gmail.com>
2016-11-03 17:27:23 +0800

    update 11.3

---
+++ zhuanti/qanda.js
@@ -0,0 +1,144 @@
+/**
+ * Created by chendehang on 2016/11/3.
+ */
+function answer01(){
+
+    var daan="答：根据2016年安排，预计2017年高职院校单独招生在2017年2月底3月初报名，3月26日考试，4月份公布录取结果（具体时间需以省教育厅公布的2017年安排为准）。考生参加单独招生考试被录取后，相应高校的官方网站上会进行公布。考生被高职院校录取后无需再参加高考。"
+    document.getElementById('answer').innerHTML=daan
+
+    document.getElementById("answer01").style.background="none"
+    document.getElementById("answer02").style.background="none"
+    document.getElementById("answer03").style.background="none"
+    document.getElementById("answer03").style.background="none"
+    document.getElementById("answer04").style.background="none"
+    document.getElementById("answer05").style.background="none"
+    document.getElementById("answer06").style.background="none"
+    document.getElementById("answer07").style.background="none"
+    document.getElementById("answer08").style.background="none"
+
+    document.getElementById("answer01").style.background="yellowgreen"
+}
+
+function answer02(){
+    document.getElementById('answer').innerHTML='答：单独招生和普通高考统考录取一样，都是计划内招生。考生入学后享受普通高考统招学生同等待遇，统一编班学习，同时毕业。合格毕业生颁发教育部电子注册的全日制普通高等学校文凭。除了录取形式和招生时间不一样，其他完全一样。'
+    document.getElementById("answer01").style.background="none"
+    document.getElementById("answer02").style.background="none"
+    document.getElementById("answer03").style.background="none"
+    document.getElementById("answer04").style.background="none"
+    document.getElementById("answer05").style.background="none"
+    document.getElementById("answer06").style.background="none"
+    document.getElementById("answer07").style.background="none"
+    document.getElementById("answer08").style.background="none"
+    document.getElementById("answer09").style.background="none"
+
+
+    document.getElementById("answer02").style.background="yellowgreen"
+}
+
+function answer03(){
+    document.getElementById('answer').innerHTML='答：按照省教育厅和省教育考试院要求，高职单招院校将一定比例招生计划放在高职单招，专业安排原则上等同普通高考录取。高校开设的各类专业，基本参考高职单招的考生也可以选择。但是由于高职单招的对考生技能的要求，建议考生切实根据自己的情况选择合适的专业，而另一方面，真正适合相关专业的考生辅以专业训练，在高职单招中将有更多录取的机会。具体招生专业需以各高校最后公布的单招计划和招生章程为准。'
+    document.getElementById("answer01").style.background="none"
+    document.getElementById("answer02").style.background="none"
+    document.getElementById("answer03").style.background="none"
+    document.getElementById("answer04").style.background="none"
+    document.getElementById("answer05").style.background="none"
+    document.getElementById("answer06").style.background="none"
+    document.getElementById("answer07").style.background="none"
+    document.getElementById("answer08").style.background="none"
+    document.getElementById("answer09").style.background="none"
+
+
+    document.getElementById("answer03").style.background="yellowgreen"
+}
+
+function answer04(){
+    document.getElementById('answer').innerHTML='答：高职单招，是在高考之前进行。对于预估高考成绩水平在本科层次以下，或者有志于更好的高职院校希望通过系统职业教育学习开启良好职业发展的考生，高职单招实际上是增加的一次高考录取机会！通过系统的准备努力去争取机会得以实现，顺利录取则不再参加高考，提前解脱。如不能录取到理想院校可以继续参加高考，不影响继续追求梦想。'
+    document.getElementById("answer01").style.background="none"
+    document.getElementById("answer02").style.background="none"
+    document.getElementById("answer03").style.background="none"
+    document.getElementById("answer04").style.background="none"
+    document.getElementById("answer05").style.background="none"
+    document.getElementById("answer06").style.background="none"
+    document.getElementById("answer07").style.background="none"
+    document.getElementById("answer08").style.background="none"
+    document.getElementById("answer09").style.background="none"
+
+
+    document.getElementById("answer04").style.background="yellowgreen"
+}
+
+function answer05(){
+    document.getElementById('answer').innerHTML='答：（1）高考的题型和考试内容，对于专科层次考生偏难，考核内容也多，语数外三科+综合考试三科。而高职单招的文化考试只考语数外三科，更好准备。<br>（2）单招中文化考试只占一定权重，综合测试的成绩也是录取的重要依据，学习水平不高的考生不意味着综合能力不高，考生有更多机会。<br>（3）高考的录取有最低要求，即使最低层次的高职专科二批，2016年的批次控制线也是文科190分、理科180分，也即高考成绩在线下的考生基本就完全失去了高校升学的机会。而高职单招没有报名限制，高考报名成功的考生都可以参加。<br>所以，高职单招是适合考生的另一个升学捷径。'
+    document.getElementById("answer01").style.background="none"
+    document.getElementById("answer02").style.background="none"
+    document.getElementById("answer03").style.background="none"
+    document.getElementById("answer04").style.background="none"
+    document.getElementById("answer05").style.background="none"
+    document.getElementById("answer06").style.background="none"
+    document.getElementById("answer07").style.background="none"
+    document.getElementById("answer08").style.background="none"
+    document.getElementById("answer09").style.background="none"
+
+    document.getElementById("answer05").style.background="yellowgreen"
+}
+
+function answer06(){
+    document.getElementById('answer').innerHTML='答：作为国家招生录取的方式之一，高职单招也是十分严谨并且自成体系的。通过集中培训，更有针对性的准备考试，不仅对考试内容进行系统训练，也考试形式也不断适应。良好的准备会大大提供通过的可能性。我们会根据考生的情况进行全面评估，再根据学生的实际情况提供多方面指导。从往年情况看，我们的辅导和培训，超过90%的考生都取得显著效果并最终顺利升学。'
+    document.getElementById("answer01").style.background="none"
+    document.getElementById("answer02").style.background="none"
+    document.getElementById("answer03").style.background="none"
+    document.getElementById("answer04").style.background="none"
+    document.getElementById("answer05").style.background="none"
+    document.getElementById("answer06").style.background="none"
+    document.getElementById("answer07").style.background="none"
+    document.getElementById("answer08").style.background="none"
+    document.getElementById("answer09").style.background="none"
+
+
+    document.getElementById("answer06").style.background="yellowgreen"
+}
+
+function answer07(){
+    document.getElementById('answer').innerHTML='答：四川省的高职单招文化考试不分文理科，考前集训我们将完全按照四川省高考单招文化考试要求安排课程和培训内容。综合测试部分，我们将根据考生报考和规划意向，按照各高校要求系统进行综合技巧培训，由高校综合测试老师、面试专家等专业人士授课。<br>培训主要从两个方面着手，1是文化，2是综合测试。<br>文化培训即语数外课程的培训，虽然单招考试的笔试比较简单，但参加单招的同学成绩大多处于中下水平，依旧有些难度，而且单招考试的考点与高中老师讲到的考点有区别，参加单招培训可以让自己的文化水平快速超过单招考试标准！<br>综合测试即礼仪、面试技巧、专业基本素质准备等。面试我们不能轻视，往年也有很多同学甚至在面对面试官时紧张的说不出话来的情况。不管是胆量、技巧、礼仪、话术等方面考生都需客服不足准备充分，这也是我们综合测试培训的重点！'
+    document.getElementById("answer01").style.background="none"
+    document.getElementById("answer02").style.background="none"
+    document.getElementById("answer03").style.background="none"
+    document.getElementById("answer04").style.background="none"
+    document.getElementById("answer05").style.background="none"
+    document.getElementById("answer06").style.background="none"
+    document.getElementById("answer07").style.background="none"
+    document.getElementById("answer08").style.background="none"
+    document.getElementById("answer09").style.background="none"
+
+    document.getElementById("answer07").style.background="yellowgreen"
+}
+
+function answer08(){
+    document.getElementById('answer').innerHTML='答：按照四川省教育厅要求，高职单招考试是全省统一时间，考试由高校自行组织。考生在通过一段时间系统培训后，需要确定报考目标学校并最终完成报名。一般考生只能报考一所学校。考试地点一般安排在所报考的院校之内，特殊情况各校高职单招招生章程会明确标明，考生需注意。参加我们高职单招考前培训的考生，我们会帮考生梳理这些信息并提醒考生注意事项。'
+    document.getElementById("answer01").style.background="none"
+    document.getElementById("answer02").style.background="none"
+    document.getElementById("answer03").style.background="none"
+    document.getElementById("answer04").style.background="none"
+    document.getElementById("answer05").style.background="none"
+    document.getElementById("answer06").style.background="none"
+    document.getElementById("answer07").style.background="none"
+    document.getElementById("answer08").style.background="none"
+    document.getElementById("answer09").style.background="none"
+
+    document.getElementById("answer08").style.background="yellowgreen"
+}
+
+function answer09(){
+    document.getElementById('answer').innerHTML='答：我们的集训中心设立在四川大学锦城学院内。按照四川大学锦城学院的统一安排，集训考生在四川大学锦城学院内接受统一的管理，集中学习和生活。集训考生可以享受学院完善的生活服务配套，如食堂、图书馆、体育场、活动中心、银行、超市等，同时还可以充分感受大学氛围，有利于树立更好的升学目标。'
+    document.getElementById("answer01").style.background="none"
+    document.getElementById("answer02").style.background="none"
+    document.getElementById("answer03").style.background="none"
+    document.getElementById("answer04").style.background="none"
+    document.getElementById("answer05").style.background="none"
+    document.getElementById("answer06").style.background="none"
+    document.getElementById("answer07").style.background="none"
+    document.getElementById("answer08").style.background="none"
+    document.getElementById("answer09").style.background="none"
+
+    document.getElementById("answer09").style.background="yellowgreen"
+}
